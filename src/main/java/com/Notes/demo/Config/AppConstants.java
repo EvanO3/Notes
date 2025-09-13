1678010910b9f6c4f859528d4bae09d820e00165
”EvanO3” <evan.oni@hotmail.com>
2025-09-13 00:25:33 -0400

    added sorting constants

--- src/main/java/com/Notes/demo/Config/AppConstants.java
+++ src/main/java/com/Notes/demo/Config/AppConstants.java
@@ -3,4 +3,6 @@ package com.Notes.demo.Config;
 public class AppConstants {
     public static final String PAGE_NUMBER="0";
     public static final String PAGE_SIZE="20";
+    public static final String SORT_DIR="asc";
+    public static final String SORT_CATEGORY_BY="categoryId";
 }

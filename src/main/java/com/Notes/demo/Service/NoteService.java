db69684941499f7aac31036833f3ae1583aa7e06
”EvanO3” <evan.oni@hotmail.com>
2025-09-13 00:25:16 -0400

    implemented sorting

--- src/main/java/com/Notes/demo/Service/NoteService.java
+++ src/main/java/com/Notes/demo/Service/NoteService.java
@@ -6,7 +6,7 @@ import com.Notes.demo.DTOs.NotesResponse;
 public interface NoteService {
     
     NotesDTO createNote(NotesDTO notes);
-    NotesResponse getAllNotes(Integer pageNumber, Integer pageSize);
+    NotesResponse getAllNotes(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     NotesDTO updateNotes(NotesDTO notes);
     NotesDTO deleteNotes(NotesDTO notes, Long notesId);
 

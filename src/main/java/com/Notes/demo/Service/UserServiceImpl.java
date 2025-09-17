4f34f4528c86cb810418cb069fe2281cafb4c3da
”EvanO3” <evan.oni@hotmail.com>
2025-09-16 21:54:15 -0400

    Service implementation class

---
+++ src/main/java/com/Notes/demo/Service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.Notes.demo.Service;
+
+import com.Notes.demo.DTOs.UserDTO;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDTO createAccount(UserDTO user) {
+      return null;
+    }
+  
+}
